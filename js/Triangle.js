7167465ff422412c79d1249143da0a3a41db9198
Rylee Muniu <ryleemuniu3@gmail.com>
2018-08-10 15:14:27 +0300

    image added

--- js/Triangle.js
+++ js/Triangle.js
@@ -1,18 +1,16 @@
-var userInput = '2,3,4'
-var tempUserInput = prompt("Input the three sides of the triangle")
+var userInput = prompt("Input the three sides of the triangle")
 var uInput =userInput.split(',')
 var tSides = uInput.map(function(side){
   return parseInt(side)
 })
 
-if (tSides[0]===tSides[1] && tSides[0]===tSides[2] && tSides[1]===tSides[2]) {
-alert("Equilateral triangle") }
-else if (tSides[0]===tSides[1] || tSides[1] == tSides[2] || tSides[0]=== tSides[2]){
+if (tSides.length!= 3 || isNaN(tSides[0]) || isNaN(tSides[1]) || isNaN(tSides[2])){
+alert("Incorrect input")}
+else if ((tSides[0]+tSides[1])<=tSides[2]|| (tSides[1]+tSides[2])<=tSides[0]||(tSides[0]+tSides[2])<=tSides[1]){
+alert("Not a triangle")}
+else if(tSides[0]===tSides[1] && tSides[0]===tSides[2] && tSides[1]===tSides[2]) {
+alert("Equilateral triangle")}
+else if (tSides[0]===tSides[1] || tSides[1] === tSides[2] || tSides[0]=== tSides[2]){
 alert("Isosceles triangle")}
 else if (tSides[0]!=tSides[1] && tSides[1] != tSides[2] && tSides[0]!= tSides[2]){
 alert("Scalene triangle")}
-else if ((tSides[0]+tSides[1]<=tSides[2])|| (tSides[1]+tSides[2]<=tSides[0])||(tSides[0]+tSides[2]<=tSides[1])){
-alert("Not a triangle")}
-else {
-alert("Incorrect input")
-}
