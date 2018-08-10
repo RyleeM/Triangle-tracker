01f1bcb4f0609560ad3c3e39b0c710653fb93026
Rylee Muniu <ryleemuniu3@gmail.com>
2018-08-10 16:03:08 +0300

    click button added

--- js/Triangle.js
+++ js/Triangle.js
@@ -1,9 +1,9 @@
+function clickButton() {
 var userInput = prompt("Input the three sides of the triangle")
 var uInput =userInput.split(',')
 var tSides = uInput.map(function(side){
   return parseInt(side)
 })
-
 if (tSides.length!= 3 || isNaN(tSides[0]) || isNaN(tSides[1]) || isNaN(tSides[2])){
 alert("Incorrect input")}
 else if ((tSides[0]+tSides[1])<=tSides[2]|| (tSides[1]+tSides[2])<=tSides[0]||(tSides[0]+tSides[2])<=tSides[1]){
@@ -14,3 +14,4 @@ else if (tSides[0]===tSides[1] || tSides[1] === tSides[2] || tSides[0]=== tSides
 alert("Isosceles triangle")}
 else if (tSides[0]!=tSides[1] && tSides[1] != tSides[2] && tSides[0]!= tSides[2]){
 alert("Scalene triangle")}
+}
